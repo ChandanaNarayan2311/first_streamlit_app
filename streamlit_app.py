dff045f5ac5df2e52ad36522948d261807ceaf8c
ChandanaNarayan2311 <149543770+ChandanaNarayan2311@users.noreply.github.com>
2023-11-01 06:16:35 +0530

    Update streamlit_app.py
    Added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and ROckey Smoothie')
 streamlit.text('🐔 Hard-Boiled Free - Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
